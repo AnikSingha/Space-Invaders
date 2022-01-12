358490c9d234806c14bda11f3d34f46f7f4135e4
= <dcnation1000000@gmail.com>
2022-01-11 23:55:05 -0500

    add files

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/Game.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+    ],
+  },
+};
